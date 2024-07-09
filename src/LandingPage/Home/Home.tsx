dbeb2e6e3a10c1a0ef2838e78b57fa0782743653
Dinesh10731j <dinesh1122@gmail.com>
2024-07-09 23:51:38 +0545

    header added

--- src/LandingPage/Home/Home.tsx
+++ src/LandingPage/Home/Home.tsx
@@ -1,37 +1,13 @@
-import Codynn_Logo from "../../assets/Codynn_Logo.png";
+
 import donateBlood from "../../assets/donate_blood.jpg";
 import Footer from "../../Components/Footer";
-import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
    <>
 
 
-   <section className="flex justify-around items-center py-3 px-2">
 
-    <section>
-      <img src={Codynn_Logo} alt="codynn_logo" className="mt-4"/>
-    </section>
-    <section>
-    <nav>
-
-    <ul className="flex flex-col md:flex-row font-medium font-sans">
-  <Link to="/"><li className="md:mr-6">Home</li></Link>
-  <Link to="/aboutus"><li className="md:mr-6">About us</li></Link>
-  <Link to="/mission"><li className="md:mr-6">Mission</li></Link>
-</ul>
-
-</nav>
-
-    </section>
-
-
-    <section>
-        <button className="bg-[#D32F2F] text-medium md:py-3 px-4 py-2 md:px-7 rounded-md tracking-wide  ">Get Started</button>
-    </section>
-
-
-   </section>
 
 
 <section className=" py-7 px-4  md:py-12 md:px-80 ">
