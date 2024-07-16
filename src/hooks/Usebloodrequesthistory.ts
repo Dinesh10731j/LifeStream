43c21f774b7471a09eb6cf3220ec7ca8c38c4213
Dinesh10731j <dinesh1122@gmail.com>
2024-07-16 14:49:35 +0545

    manage request and delete manage request frontend completed

--- src/hooks/Usebloodrequesthistory.ts
+++ src/hooks/Usebloodrequesthistory.ts
@@ -7,7 +7,7 @@ const fetchReceiptentrequeshistory = async () => {
   try {
     const token = Cookies.get("token");
 
-    const email = Cookies.get("email") || ''; // Ensure userid is treated as string
+    const email = Cookies.get("email") || ''; 
 
     const response = await axiosInstance.get(Endpoints.Bloodrequesthistory(email), {
         headers: {
@@ -15,12 +15,12 @@ const fetchReceiptentrequeshistory = async () => {
         }
       });
     console.log('This is blod request history',response.data);
-    return response.data.data; // Assuming response.data contains the required data
+    return response.data.data; 
   } catch (error) {
     throw new Error("Failed to fetch user profile");
   }
 };
 
 export const UseReceipentrequesthistory = (_email?: string) => {
-  return useQuery({ queryKey:['bllodrequesthistory'], queryFn:fetchReceiptentrequeshistory});
+  return useQuery({ queryKey:['bloodrequesthistory'], queryFn:fetchReceiptentrequeshistory});
 };
---
+++ src/hooks/Usemanagebloodrequest.ts
@@ -0,0 +1,28 @@
+import { Endpoints } from "../api/Apendpoints";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../api/axiosInterceptor";
+import Cookies from "js-cookie";
+const {Managebloodrquest} = Endpoints;
+
+const fetchManageBloodRequest = async  ()=>{
+    const token = Cookies.get("token");
+
+    const response = await axiosInstance.get(Managebloodrquest,{
+        headers:{
+            Authorization:`Bearer ${token}`
+
+        }
+
+
+    
+    });
+
+
+return response.data.data;
+
+}
+
+
+ export const UseManageBloodRequest = ()=>{
+    return useQuery({queryKey:['managebloodrequest'],queryFn:fetchManageBloodRequest})
+}
