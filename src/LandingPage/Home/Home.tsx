60e89f057fd2f97e175264054d7d3a7c8d9b4dbc
Dinesh10731j <dinesh1122@gmail.com>
2024-07-17 23:45:57 +0545

    landingpage animated started

--- src/LandingPage/Home/Home.tsx
+++ src/LandingPage/Home/Home.tsx
@@ -1,26 +1,50 @@
 import donateBlood from "../../assets/donate_blood.jpg";
 import Footer from "../../Components/Footer";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <>
       <section className=" py-7 px-4  md:py-12 md:px-80 ">
-        <h1 className="md:text-6xl text-4xl">Welcome to Lifestream</h1>
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", stiffness: 50 }}
+          className="md:text-6xl text-4xl"
+        >
+          Welcome to Lifestream
+        </motion.h1>
 
-<h3 className="md:text-2xl text-xl py-4 px-2 text-justify text-wrap "> Save Lives, Donate Blood Every drop counts</h3>
-        <h1 className="text-justify text-md text-wrap md:text-2xl font-sans">
-          At Lifestream, we are dedicated to ensuring a safe and sufficient blood supply
-          for those in need. Your blood donation can make a life-saving
-          difference for patients undergoing surgeries, battling cancer, and
-          facing unexpected emergencies.
-        </h1>
+        <motion.h3 className="md:text-2xl text-xl py-4 px-2 text-justify text-wrap " initial={{opacity:0,x:300}}
+        animate={{opacity:1,x:0}}
+        transition={{type:'just',delay:0.3}}
+        >
+          {" "}
+          Save Lives, Donate Blood Every drop counts
+        </motion.h3>
+        <motion.h1 className="text-justify text-md text-wrap md:text-2xl font-sans" initial={{x:-300,opacity:0}} animate={{x:0,opacity:1}}
+        transition={{type:'spring',delay:0.5,stiffness:300}}
+        >
+          At Lifestream, we are dedicated to ensuring a safe and sufficient
+          blood supply for those in need. Your blood donation can make a
+          life-saving difference for patients undergoing surgeries, battling
+          cancer, and facing unexpected emergencies.
+
+          
+        </motion.h1>
       </section>
 
-      <section className="flex flex-col justify-center items-center py-2 px-2">
-        <img
+      <section
+        className="flex flex-col justify-center items-center py-2 px-2"
+      
+      >
+        <motion.img
           src={donateBlood}
           alt="blood_image"
           className="h-[500px] w-[500px]"
+          initial={{ opacity: 0, transitionBehavior: "revert", y:-50,}} animate={{opacity:1,y:0} }
+
+          transition={{type:'spring',stiffness:300}}
         />
       </section>
 
