826eed7bc22dd44533ae553325b3f8eae6b55d19
Dinesh10731j <dinesh1122@gmail.com>
2024-07-20 16:54:03 +0545

    Admin can see  the history of an donor

--- src/Viewuserhistory/Viewuserhistory.tsx
+++ src/Viewuserhistory/Viewuserhistory.tsx
@@ -1,26 +1,67 @@
 import { useParams } from 'react-router-dom';
 import { UseViewHistory } from '../hooks/Useviewhistory';
 import Bgbubble from '../Components/Bgbubble';
+import { CircularProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography} from '@mui/material';
+
+interface HistoryType {
+  _id: string;
+  fullName: string;
+  email: string;
+  status: string;
+  bloodGroup: string;
+}
+
 const Viewuserhistory = () => {
   const { id } = useParams();
   const { data, error, isLoading } = UseViewHistory(id || '');
-  console.log(data);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className='flex flex-col justify-center items-center py-12'><CircularProgress size={30} color='primary' /></div>;
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div className='text-red-500'>Error: {error.message}</div>;
   }
 
   return (
     <div>
-      <Bgbubble/>
-    <section className='flex flex-col items-center justify-end'>
-      <h1>This is a user profile</h1>
+      <Bgbubble />
+      <section className='flex flex-col items-center justify-end'>
+        <Typography variant="h4" gutterBottom>
+          User Profile History
+        </Typography>
 
-    </section>
+        <TableContainer component={Paper} style={{ maxWidth: '90%', marginTop: '20px' }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Blood Group</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data && data.length > 0 ? (
+                data.map((history: HistoryType) => (
+                  <TableRow key={history._id}>
+                    <TableCell>{history.fullName}</TableCell>
+                    <TableCell>{history.email}</TableCell>
+                    <TableCell>{history.status}</TableCell>
+                    <TableCell>{history.bloodGroup}</TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={4} align="center">
+                    Data not found
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </section>
     </div>
   );
 };
